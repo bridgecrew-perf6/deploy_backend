a989a855608c433420640ea7543795dcab6029d8
tushar <tushar.singhal,mec19@itbhu.ac.in>
2022-05-29 00:04:26 +0530

    ch

--- eng_backend/celery.py
+++ eng_backend/celery.py
@@ -1,12 +1,12 @@
 import os
 from celery import Celery
-
+from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eng_backend.settings')
 
 app = Celery('eng_backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.update(BROKER_URL='amqps://gwewsfku:1Ek9hM3tes4wOaXJQ0D-YNbndmjGX2WO@sparrow.rmq.cloudamqp.com/gwewsfku')
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
